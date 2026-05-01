f9b0df0af6a343107e02ade2b797f4274c4a7525
nishankvarshney07-ai <nishankvarshney07@gmail.com>
2026-05-02 00:50:07 +0530

    random

--- node.js
+++ node.js
@@ -1,19 +1,14 @@
+const { log } = require('console');
 const fs=require('fs');
 const http=require('http');
-
-
+const url=require('url');
 
 //server
 const server  = http.createServer((req,res)=>{
-    console.log(req);
+    console.log(req.url);
+    
     res.end('hello client');
 });
-server.listen(8000,'127.0.0.1',(()=>{
+server.listen(8000,'127.0.0.1',()=>{
     console.log('listening to request to 8000');
-}))
-
-
-
-
-
-
+});
