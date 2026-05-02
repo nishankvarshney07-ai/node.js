81106eba906bbbf5d519893f1cc656af153deac1
nishankvarshney07-ai <nishankvarshney07@gmail.com>
2026-05-02 23:34:18 +0530

    second commit

--- node.js
+++ node.js
@@ -5,9 +5,20 @@ const url=require('url');
 
 //server
 const server  = http.createServer((req,res)=>{
-    console.log(req.url);
+    //console.log(req.url);
+    const pathName = req.url;
+    if(pathName==='/' || pathName==='/overview'){
+        res.end('This is the Overview');
+    }else if(pathName === '/product'){
+        res.end('This is the product');
+    }else if(pathName==='/api'){
+        
+        res.end('his is api');
+    }else{
+        res.end('page not found');
+    }
     
-    res.end('hello client');
+    //res.end('hello client');
 });
 server.listen(8000,'127.0.0.1',()=>{
     console.log('listening to request to 8000');
